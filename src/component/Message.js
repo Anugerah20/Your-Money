0fe3a4e99d0d242e714ace9df356c8384e7295df
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-01-11 21:48:45 +0700

    add message

---
+++ src/component/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Message extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <p className="alert-p">Maaf, Data masih kosong....</p>;
+  }
+}
+
+export default Message;
